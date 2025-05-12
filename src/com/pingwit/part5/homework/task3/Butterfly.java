03538f9a1d16b15eae6483e77b359fbde59beda8
Elena <elena.sommerfeld@yahoo.de#>
2025-05-12 19:32:46 +0200

    fixed remarks

--- src/com/pingwit/part5/homework/task3/Butterfly.java
+++ src/com/pingwit/part5/homework/task3/Butterfly.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class Butterfly {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int height;
 
-        System.out.print("Please enter the size of the butterfly: ");
-        int height = scanner.nextInt();
+        while (true) {
+            System.out.print("Please enter the size of the butterfly: ");
+            height = scanner.nextInt();
+
+            if (height % 2 == 0) {
+                break;
+            } else {
+                System.out.println("The size of your butterfly should be an even number!");
+            }
+        }
+
         int width = height;
         int center = height / 2;
 
@@ -15,6 +25,7 @@ public class Butterfly {
             for (int j = 0; j <= width; j++) {
                 // Это очень необычное решение. Мне нравится его однострочность, но читать его сложновато.
                 // Есть желание поковырять еще и заодно в середине сделать 1 строку без пробелов вместо 2? Можно оставить как есть, я вообще не против
+                // Проблемы с пробелом решила циклом while. Остальное пока оставлю так ))
                 if ((j - i <= 0 && j + i <= height && j <= center) || (j + i >= height && j - i >= 0 && j >= center))
                     System.out.print("*");
                 else
