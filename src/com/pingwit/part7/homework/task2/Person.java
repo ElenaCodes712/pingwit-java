b8d2fe854300731724b3bd05a7fe9506cb2368d4
Alexander Fomin <rezviybelorus@gmail.com>
2025-05-23 09:05:10 +0200

    [Check] Homework 7

--- src/com/pingwit/part7/homework/task2/Person.java
+++ src/com/pingwit/part7/homework/task2/Person.java
@@ -5,7 +5,7 @@ public class Person {
     String name;
     int age;
 
-    static {
+    static { //Вопрос, если в task3 в массив добавить еще одного Person, то сколько раз будет выведено сообщение из static блоков на экран? Мы ожидаем 3, но есть подозрение что 3 не будет. Осталось выяснить почему :)
         System.out.println("You created a new instance of the class");
     }
 
@@ -14,7 +14,7 @@ public class Person {
         this.age = age;
     }
 
-    static {
+    static { // это дубликат static блока выше. статик блоки инициализации и не статик должны располагаться рядом. Конструкторы тоже рядом, без чередований
         System.out.println("You created a new instance of the class");
     }
 
--- src/com/pingwit/part7/homework/task4/InsuranceCosts.java
+++ src/com/pingwit/part7/homework/task4/InsuranceCosts.java
@@ -11,24 +11,26 @@ public class InsuranceCosts {
         int engineVolume = scanner.nextInt();
 
         System.out.println("Были ДТП за прошлый год (да = true / нет = false)? ");
-        Boolean accident = scanner.nextBoolean();
+        Boolean accident = scanner.nextBoolean(); // метод nextBoolean() возвращает boolean, т.е. примитив. Поэтому правильнее будет Boolean accident -> boolean accident, хотя это не критично
 
         System.out.println("Страховались у нас (да = true / нет = false)? ");
-        Boolean formerCustomer = scanner.nextBoolean();
+        Boolean formerCustomer = scanner.nextBoolean(); // метод nextBoolean() возвращает boolean, т.е. примитив. Поэтому правильнее будет Boolean accident -> boolean accident, хотя это не критично
 
         double engineVolumeRate = 0.25;
         double insuranceCosts = engineVolume * engineVolumeRate;
-        double costsWithDiscount;
-        double totalCosts;
+        double costsWithDiscount; // давай уберем эту переменную и постепенно будем изменять insuranceCosts
+        double totalCosts; // давай уберем эту переменную и постепенно будем изменять insuranceCosts
 
+        // accident -> hasAccidents будет ярче указывать на то, что это скорее всего переменная boolean.
+        // внутри условия if должно быть boolean выражение, твой код будет работать корректно, но переменная accident типа boolean, поэтому твой код можно упростить до if (accident)
         if (accident == true) {
-            double discountRate = 1.2;
+            double discountRate = 1.2; // discountRate звучит как скидка, это скорее accidentAdditionalRate
             costsWithDiscount = insuranceCosts * discountRate;
         } else {
             costsWithDiscount = insuranceCosts;
         }
 
-        if (formerCustomer == true) {
+        if (formerCustomer == true) { // if (formerCustomer == true) -> if (formerCustomer). Подробнее описал выше
             double customerDiscount = 0.8;
             totalCosts = costsWithDiscount * customerDiscount;
         } else {
