e75e3e87373ecd1f2c98f342b4cf432638f97c64
Alexander Fomin <rezviybelorus@gmail.com>
2025-07-21 20:21:35 +0200

    [Check] Homework 9

--- src/com/pingwit/part9/homework/task2/PersonDataCheck.java
+++ src/com/pingwit/part9/homework/task2/PersonDataCheck.java
@@ -2,12 +2,12 @@ package com.pingwit.part9.homework.task2;
 
 import java.time.LocalDate;
 
-public class PersonDataCheck {
+public class PersonDataCheck { // PersonDataCheck -> PersonDataCheckService
     public void datingSuggestion(Person person) {
 
         int currentYear = LocalDate.now().getYear();
         int age = currentYear - person.getBirthYear();
-        int legalAge = 18;
+        int legalAge = 18; // это хороший кандидат на константу класса
 
         if (age >= legalAge && person.getMaritalStatus() == MaritalStatus.SINGLE) {
             if (person.getGender() == Gender.FEMALE) {
