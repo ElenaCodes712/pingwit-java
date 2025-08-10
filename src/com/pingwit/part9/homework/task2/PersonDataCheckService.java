b3253bd40ccf8fdba6b4d7e623b72792e7e438c9
Alexander Fomin <rezviybelorus@gmail.com>
2025-08-10 22:14:53 +0200

    [Check] Homework 9

--- src/com/pingwit/part9/homework/task2/PersonDataCheckService.java
+++ src/com/pingwit/part9/homework/task2/PersonDataCheckService.java
@@ -3,7 +3,7 @@ package com.pingwit.part9.homework.task2;
 import java.time.LocalDate;
 
 public class PersonDataCheckService {
-    private static final int LEGAL_AGE = 18;
+    private static final int LEGAL_AGE = 18; //после полей класса ставим абзац
     public void datingSuggestion(Person person) {
 
         int currentYear = LocalDate.now().getYear();
